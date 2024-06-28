5cb84ce8fdd4557e24e2094d6042088b9452c31c
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2024-06-28 11:13:38 -0300

    #4 - Cria esqueleto para a classe banco de dados

---
+++ havazon/src/models/Database.java
@@ -0,0 +1,12 @@
+package models;
+
+public class Database {
+    private Database() {
+    }
+
+    /*
+     * private static ArrayList<Vendas> vendas = new ArrayList<Vendas>();
+     * private static ArrayList<Produtos> produtos = new ArrayList<Produtos>();
+     * private static ArrayList<Clientes> clientes = new ArrayList<Clientes>();
+     */
+}
