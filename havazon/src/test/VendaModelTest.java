15ee2064188e6015396c89f19acdf2172868f220
ErickMVdO <erickmelovidal@gmail.com>
2024-07-07 23:08:07 -0300

    novos casos de testes para vendas

--- havazon/src/test/VendaModelTest.java
+++ havazon/src/test/VendaModelTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertEquals;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,40 +30,110 @@ public class VendaModelTest {
 
     @Parameters
     public static Collection<Object[]> getParameters() {
-        ClienteModel cliente1 = new ClienteModel(new EnderecoModel(true, EstadoEnum.BA),
-                new CartaoModel(" 4296 13XX XXXX XXXX "));
-        // Cliente especial
-        cliente1.setValorTotalComprasMensal(101);
 
-        LocalDateTime dateTime1 = LocalDateTime.of(2024, 7, 2, 10, 0);
+        ClienteModel cliente1 = new ClienteModel(new EnderecoModel(false, EstadoEnum.GO), new CartaoModel("1234 1234 1234 1234"));
+        ClienteModel cliente2 = new ClienteModel(new EnderecoModel(true, EstadoEnum.RS), new CartaoModel("4296 1334 1234 1234"));
+        ClienteModel cliente3 = new ClienteModel(new EnderecoModel(true, EstadoEnum.DF), new CartaoModel("4296 1234 1234 1234"));
+        ClienteModel cliente4 = new ClienteModel(new EnderecoModel(false, EstadoEnum.PE), new CartaoModel("4296 1334 4321 4321"));
 
-        List<ProdutoModel> produtos1 = Arrays.asList(
-                new ProdutoModel(1, "Produto 1", 50.0, "peça"),
-                new ProdutoModel(2, "Produto 2", 60.0, "peça"));
+        cliente2.assinaturaPrime();
+        cliente3.setValorTotalComprasMensal(120.00);
+        cliente4.setValorTotalComprasMensal(120.00);
+        cliente4.assinaturaPrime();
 
-        VendaModel vendaModel1 = new VendaModel(cliente1, dateTime1, produtos1);
+        ProdutoModel caneta = new ProdutoModel(101, "Caneta Esferográfica", 1.50, "unidade");
+        ProdutoModel notebook = new ProdutoModel(102, "Notebook 15.6\" 8GB RAM", 3500.00, "peça");
+        ProdutoModel cafe = new ProdutoModel(103, "Café em Grãos 1kg", 25.90, "kg");
+        ProdutoModel tv = new ProdutoModel(104, "TV 42\" LED Full HD", 2300.00, "peça");
+        ProdutoModel tecido = new ProdutoModel(105, "Tecido Algodão 1m", 12.00, "metro");
+
+        VendaModel venda1 = new VendaModel(cliente1, LocalDateTime.parse("2024-07-06T17:57:42.978223"), Arrays.asList(caneta, notebook));
+        VendaModel venda2 = new VendaModel(cliente2, LocalDateTime.parse("2024-04-05T17:57:42.978223"), Arrays.asList(tv, notebook));
+        VendaModel venda3 = new VendaModel(cliente3, LocalDateTime.parse("2024-04-05T17:57:42.978223"), Arrays.asList(tecido));
+        VendaModel venda4 = new VendaModel(cliente4, LocalDateTime.parse("2024-04-05T17:57:42.978223"), Arrays.asList(tecido, cafe, caneta, tv));
 
         String expectedOutput1 = "========================================\n" +
                 "NOTA FISCAL\n" +
                 "========================================\n" +
                 "Cliente ID: 1\n" +
-                "Tipo Do Cliente: ESPECIAL\n" +
-                "Data: 2024-07-02 10:00:00\n" +
+                "Tipo Do Cliente: PADRAO\n" +
+                "Data: 2024-07-06 17:57:42\n" +
                 "----------------------------------------\n" +
                 "PRODUTOS\n" +
-                "- Produto 1\n" +
-                "- Produto 2\n" +
+                "- Caneta Esferográfica\n" +
+                "- Notebook 15.6\" 8GB RAM\n" +
+                "----------------------------------------\n" +
+                "Desconto: 0.00%\n" +
+                "Saldo de Cashback: 0.00\n" +
+                "Frete: 13.00\n" +
+                "Municipal: 140.58\n" +
+                "ICMS: 421.74\n" +
+                "Valor Total: 4076.82\n" +
+                "========================================\n";
+
+            String expectedOutput2 = "========================================\n" +
+                "NOTA FISCAL\n" +
+                "========================================\n" +
+                "Cliente ID: 2\n" +
+                "Tipo Do Cliente: PRIME\n" +
+                "Data: 2024-04-05 17:57:42\n" +
+                "----------------------------------------\n" +
+                "PRODUTOS\n" +
+                "- TV 42\" LED Full HD\n" +
+                "- Notebook 15.6\" 8GB RAM\n" +
                 "----------------------------------------\n" +
                 "Desconto: 10.00%\n" +
                 "Saldo de Cashback: 0.00\n" +
-                "Frete: 10.50\n" +
-                "Municipal: 4.38\n" +
-                "ICMS: 13.14\n" +
-                "Valor Total: 127.02\n" +
+                "Frete: 10.00\n" +
+                "Municipal: 209.20\n" +
+                "ICMS: 627.60\n" +
+                "Valor Total: 6066.80\n" +
+                "========================================\n";
+
+            String expectedOutput3 = "========================================\n" +
+                "NOTA FISCAL\n" +
+                "========================================\n" +
+                "Cliente ID: 3\n" +
+                "Tipo Do Cliente: ESPECIAL\n" +
+                "Data: 2024-04-05 17:57:42\n" +
+                "----------------------------------------\n" +
+                "PRODUTOS\n" +
+                "- Tecido Algodão 1m\n" +
+                "----------------------------------------\n" +
+                "Desconto: 10.00%\n" +
+                "Saldo de Cashback: 0.00\n" +
+                "Frete: 3.50\n" +
+                "Municipal: 0.00\n" +
+                "ICMS: 2.57\n" +
+                "Valor Total: 16.87\n" +
+                "========================================\n";
+
+            String expectedOutput4 = "========================================\n" +
+                "NOTA FISCAL\n" +
+                "========================================\n" +
+                "Cliente ID: 4\n" +
+                "Tipo Do Cliente: PRIME_ESPECIAL\n" +
+                "Data: 2024-04-05 17:57:42\n" +
+                "----------------------------------------\n" +
+                "PRODUTOS\n" +
+                "- Tecido Algodão 1m\n" +
+                "- Café em Grãos 1kg\n" +
+                "- Caneta Esferográfica\n" +
+                "- TV 42\" LED Full HD\n" +
+                "----------------------------------------\n" +
+                "Desconto: 20.00%\n" +
+                "Saldo de Cashback: 0.00\n" +
+                "Frete: 18.00\n" +
+                "Municipal: 75.58\n" +
+                "ICMS: 226.74\n" +
+                "Valor Total: 2191.84\n" +
                 "========================================\n";
 
         return Arrays.asList(new Object[][] {
-                { vendaModel1, expectedOutput1 },
+                { venda1, expectedOutput1 },
+                { venda2, expectedOutput2 },
+                { venda3, expectedOutput3 },
+                { venda4, expectedOutput4 },
 
         });
     }
