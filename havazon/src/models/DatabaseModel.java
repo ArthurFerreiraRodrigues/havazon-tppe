ffcd6d7ffe918314494ca9a5654c48c02ff2b493
augustocrmg <augusto.duraes33@gmail.com>
2024-07-07 22:21:41 -0300

    Atualização lógica banco de dados

--- havazon/src/models/DatabaseModel.java
+++ havazon/src/models/DatabaseModel.java
@@ -6,14 +6,20 @@ public class DatabaseModel {
     private DatabaseModel() {
     }
 
-    /*
-     * private static ArrayList<Vendas> vendas = new ArrayList<Vendas>();
-     * private static ArrayList<Produtos> produtos = new ArrayList<Produtos>();
-     */
+    private static ArrayList<VendaModel> vendas = new ArrayList<VendaModel>();
+    private static ArrayList<ProdutoModel> produtos = new ArrayList<ProdutoModel>();
+
     private static ArrayList<ClienteModel> clientes = new ArrayList<ClienteModel>();
 
     public static ArrayList<ClienteModel> getClientes() {
         return clientes;
     }
 
+    public static ArrayList<VendaModel> getVendas() {
+        return vendas;
+    }
+
+    public static ArrayList<ProdutoModel> getProdutos() {
+        return produtos;
+    }
 }
